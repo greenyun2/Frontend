8535662a510f36a67d7bbc3bfb4f5cbee91462a7
greenyun2 <126546289+greenyun2@users.noreply.github.com>
2023-03-20 18:25:17 +0900

    Add files via upload

---
+++ 3주차수업/0320모바일/js/script.js
@@ -0,0 +1,78 @@
+$(function() {
+  $('.gnb-include').load('/include/gnb.html');
+  $('.main-header-include').load('/include/main-header.html');
+
+  /*category accordion*/
+  $('.category-accordion .detail').eq(0).show()
+  $('.category-accordion .title').click(function() {
+    $(this).next().show();
+    $(this).siblings('.category-accordion .title').next().stop().slideUp();
+
+    $(this).addClass('active')
+    $(this).siblings('.category-accordion .title').removeClass('active');
+  });
+
+    /*recent search result*/
+  $('.search-recent .btn-clear').click(function() {
+    $(this).parent().hide();
+  });
+  $('.search-recent .btn-all-clear').click(function() {
+    $('.search-recent .item').hide();
+  });
+
+  /*Front Slider*/
+  $('.front-slider').slick({
+    slidesToShow: 1,
+    dots: false, 
+    arows:true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slideToScroll: 1
+  });
+  
+
+  /*Cart Items*/
+  $('.cart-content .btn-all-clear').click(function() {
+    $('.cart-items').hide();
+  });
+  $('.cart-content .btn-clear').click(function() {
+    $(this).parent().parent().hide();
+  });
+
+
+  /*Goods Suggestion Slider*/
+  $('.goods-suggestion-items').slick({
+    slidesToShow: 3,
+    dots: false,
+    arrows: true,
+    slideToScroll: 2
+  });
+
+  /*wish*/
+  $('.wish').click(function() {
+    $(this).toggleClass('active')
+  });
+
+  /*front*/
+  $('.btn-view-more').click(function() {
+    $('.company-info-more').toggle()
+  });
+
+  // Goods Detail Slider //
+  $('.goods-detail-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: true,
+    slideToScroll: 1
+  });
+});
+
+
+
+
+
+
+
+
+
+
