36157ff0738d99b0d889b5e3302578943052f91f
greenyun2 <126546289+greenyun2@users.noreply.github.com>
2023-03-22 18:24:17 +0900

    Add files via upload

---
+++ 4주차수업/0322Responsive webpage_ver1/custom.js
@@ -0,0 +1,34 @@
+$(function() {
+  $('.trigger').click(function() {
+    $(this).toggleClass('active');
+    $('.gnb').toggleClass('active');
+  });
+  $('section, .menu a').click(function() {
+    $('.gnb').removeClass('active');
+    $('.trigger').removeClass('active');
+  });
+
+  /* Change CSS Scroll */
+  $(window).scroll(function() {
+    if($(window).scrollTop() > 50) {
+      $('header, .gototop').addClass('active')
+      } else {
+        $('header, .gototop').removeClass('active');
+      }
+  });
+
+  /* Smooth Scrollring */
+  $('.menu a, .gototop').click(function(e) {
+    $.scrollTo(this.ha+sh || 0, 900);
+  });
+
+  /* Slick Slider : Plan > History */
+  $('.history-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  }
+  )
+});
